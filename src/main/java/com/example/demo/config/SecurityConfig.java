fd5df6b273e2e9988794ee50597e39bbe006900d
nanineelapu <neelapunani2580@gmail.com>
2025-05-16 23:09:25 +0530

    backend run final

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/auth/admin/register").authenticated()
                 .requestMatchers("/api/admin/**").authenticated()
                 .requestMatchers("/api/user/recharge", "/api/user/plans").permitAll()
-                .requestMatchers("/api/user/**").authenticated() // instead of permitAll()
+                .requestMatchers("/api/user/**").permitAll() // instead of permitAll()
                 .anyRequest().authenticated()
             )
             .headers(headers -> headers
